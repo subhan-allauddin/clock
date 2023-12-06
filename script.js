6d7c376d5de1e526a2e33fb922091e0db6339462
subhan-allauddin <subhanallauddin@gmail.com>
2023-12-06 13:20:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+let Day = document.getElementById("Day");
+
+
+setInterval(()=>{
+    let currentTime = new Date();
+Day.innerHTML = currentTime.getDay();
+hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+sec.innerHTML = currentTime.getSeconds();
+},1000)
+if(day = 1){
+    document.write("Monday")
+}
+
+
+
+
